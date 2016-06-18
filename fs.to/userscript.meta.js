d23c515d02ea5bc681329d4ce6c5c7bfb110645d
quc <quc@github.com>
2016-06-18 14:30:38 +0300

    testing userscript update

--- fs.to/userscript.meta.js
+++ fs.to/userscript.meta.js
@@ -2,8 +2,8 @@
 // @name FS.TO / BRB.TO Page Multiplier
 // @description Load more files per page
 // @homepageURL https://github.com/quc
-// @updateURL https://www.example.com/userscript.meta.js
-// @downloadURL https://www.example.com/userscript.js
+// @updateURL https://raw.githubusercontent.com/quc/userscripts/master/fs.to/userscript.meta.js
+// @downloadURL https://raw.githubusercontent.com/quc/userscripts/master/fs.to/userscript.js
 // @version 1.0.0
 // @author quc
 // @date 2016-06-18
